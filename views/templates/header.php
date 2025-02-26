dbc06f5b65e6636f1138033f5d01b2684cdccad5
Ngurah Gde Rheino Darma Tenaya Perean <rheino.3223@gmail.com>
2025-02-26 10:15:38 +0800

    ndak bisa aku pusing kleng

--- views/templates/header.php
+++ views/templates/header.php
@@ -13,7 +13,7 @@
     <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
         <div class="container">
-            <a class="navbar-brand" href="index.html">Furni<span>.</span></a>
+            <a class="navbar-brand" href="<?=BASE_URL?>">Furni<span>.</span></a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -22,7 +22,7 @@
             <div class="collapse navbar-collapse" id="navbarsFurni">
                 <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="<?=BASE_URL?>">Home</a>
                     </li>
                     <li><a class="nav-link" href="shop.html">Shop</a></li>
                     <li><a class="nav-link" href="about.html">About us</a></li>
@@ -32,8 +32,8 @@
                 </ul>
 
                 <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-                    <li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
-                    <li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>
+                    <li><a class="nav-link" href="#"><img src="<?=BASE_URL?>public/images/user.svg"></a></li>
+                    <li><a class="nav-link" href="cart.html"><img src="<?=BASE_URL?>public/images/cart.svg"></a></li>
                 </ul>
             </div>
         </div>
